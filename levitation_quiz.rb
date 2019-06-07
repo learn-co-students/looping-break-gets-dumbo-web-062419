e93cd954f78996ee75a19cd0babbdd6402c5d8f9
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-07 16:05:11 -0400

    use break and gets

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,12 @@
 
 def levitation_quiz
-	#your code here
+	loop do
+		spell = "Wingardium Leviosa"
+		puts "What is the spell that enacts levitation?"
+		# spell = gets.chomp
+		break if spell == "Wingardium Leviosa"	
+	end
+	puts "You passed the quiz!"
 end
 
-
+levitation_quiz
